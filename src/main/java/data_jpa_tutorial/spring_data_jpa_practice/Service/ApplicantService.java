0a2097bea8fb754b167a60489dcbe8ad5ea21090
Ramanna <ramannaagre@gmail.com>
2026-02-25 00:02:44 +0530

    add metrics and group health indicator

--- src/main/java/data_jpa_tutorial/spring_data_jpa_practice/Service/ApplicantService.java
+++ src/main/java/data_jpa_tutorial/spring_data_jpa_practice/Service/ApplicantService.java
@@ -3,6 +3,11 @@ package data_jpa_tutorial.spring_data_jpa_practice.Service;
 import data_jpa_tutorial.spring_data_jpa_practice.Entity.Applicant;
 import data_jpa_tutorial.spring_data_jpa_practice.Repository.ApplicantRepository;
 import data_jpa_tutorial.spring_data_jpa_practice.Repository.ApplicationPagingAndSortingRepository;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,42 +25,46 @@ public class ApplicantService {
 
     private ApplicationPagingAndSortingRepository applicationPagingAndSortingRepository;
 
-    public ApplicantService(ApplicantRepository applicantRepository, ApplicationPagingAndSortingRepository applicationPagingAndSortingRepository){
+    private Counter counter;
+    private Timer timer;
+    private DistributionSummary summary;
+
+    public ApplicantService(ApplicantRepository applicantRepository, ApplicationPagingAndSortingRepository applicationPagingAndSortingRepository, MeterRegistry registry){
         this.applicantRepository = applicantRepository;
         this.applicationPagingAndSortingRepository = applicationPagingAndSortingRepository;
+        this.counter = registry.counter("applicant.counter");
+        this.timer = registry.timer("applicant.timer");
+        this.summary = registry.summary("applicant.summary");
     }
 
-    //get applicant
     public Optional<Applicant> getApplicant(Long id){
+        counter.increment();
         return applicantRepository.findById(id);
     }
 
-    //get all applicants
     public List<Applicant> getAllApplicants(){
-        return applicantRepository.findAll();
+        return timer.record(() -> applicantRepository.findAll());
     }
 
-    //add applicant
+    @Timed
     public Applicant addApplicant(Applicant applicant){
         return applicantRepository.save(applicant);
     }
 
-    //delete applicant
     public void deleteApplicant(Long id){
         applicantRepository.deleteById(id);
     }
 
-    //pagination
     public Iterable<Applicant> getApplicantWithPagination(int page, int size){
         return applicationPagingAndSortingRepository.findAll(PageRequest.of(page, size));
     }
 
-    //query methods
     public List<Applicant> findByStatus(String status){
-         return applicantRepository.findByStatus(status);
+        List<Applicant> applicants = applicantRepository.findByStatus(status);
+        summary.record(applicants.size());
+        return applicants;
     }
 
-    //query annotation
     public List<Applicant> findByPartialName(String name){
         return applicantRepository.findApplicantsByPartialName(name);
     }
